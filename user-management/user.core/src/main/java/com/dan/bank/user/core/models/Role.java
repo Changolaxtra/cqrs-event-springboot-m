d5180cee3f23aa7e0d264793289f9494e613f40a
Daniel Rojas <dan.rojasx@gmail.com>
2022-12-21 12:10:48 -0600

    Adding user models

---
+++ user-management/user.core/src/main/java/com/dan/bank/user/core/models/Role.java
@@ -0,0 +1,13 @@
+package com.dan.bank.user.core.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
+    READ_PRIVILEGE, WRITE_PRIVILEGE;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
